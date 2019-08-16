09be3af3818b701fc31d04801e14c499d97c1dc6
Danny Li <hello@dannyli.nl>
2019-08-16 22:27:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## Bot.tf stats on backpack.tf
+
+This user script integrates Bot.tf stats with the backpack.tf website.
+
+## Installation
+
+1. Install [Greasemonkey (Firefox)](http://www.greasespot.net/) or [Tampermonkey (Chrome)](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo).
+2. Open the [script in raw view](https://github.com/dyhli/plugin-bottf-stats-bptf/raw/master/bottf_stats_bptf.user.js).
+3. Install.
+
+## Known issues
+- Doesn't really work with default weapons (yet), can't be bothered to fix it right now.
+
+## Images
+
+#### Item history page
+![Item history page](https://i.imgur.com/oiFQoY2.png)
+
+#### Classifieds page
+![Classifieds page](https://i.imgur.com/llaEGLF.png)
+
+### Disclaimer
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+### License
+MIT
---
+++ bottf_stats_bptf.user.js
@@ -0,0 +1,101 @@
+// ==UserScript==
+// @name            Bot.tf stats backpack.tf integration
+// @namespace       https://bot.tf
+// @description     This user script integrates Bot.tf stats with the backpack.tf website
+
+// @homepageURL     https://github.com/dyhli/plugin-bottf-stats-bptf
+// @supportURL      https://github.com/dyhli/plugin-bottf-stats-bptf/issues
+// @downloadURL     https://github.com/dyhli/plugin-bottf-stats-bptf/raw/master/bottf_stats_bptf.user.js
+// @updateURL       https://github.com/dyhli/plugin-bottf-stats-bptf/raw/master/bottf_stats_bptf.user.js
+
+// @version         0.1
+// @author          Yuan Hao "Danny" Li <danny@exploriment.io>
+
+// @match           https://backpack.tf/stats/*
+// @match           https://backpack.tf/item/*
+// @run-at          document-end
+// @grant           none
+// ==/UserScript==
+
+(function($) {
+    'use strict';
+
+    const Item = $('.stats-header-item .item, .history-sheet .item-list .item');
+
+    if (Item.length > 0)
+    {
+        const ItemData = {
+            def: Item.data('defindex'),
+            q: Item.data('quality'),
+            craft: Item.data('craftable')
+        };
+
+        if (Item.data('effect_name'))
+        {
+            ItemData.ef = Item.data('effect_name');
+        }
+
+        if (Item.data('australium'))
+        {
+            ItemData.aus = '1';
+        }
+
+        if (Item.data('ks_tier'))
+        {
+            ItemData.ks = Item.data('ks_tier');
+        }
+
+        $('#classifieds').append(`
+            <a class="btn btn-default" href="${generateSnapshotLink(ItemData)}" target="_blank"><i class="fa fa-history fa-fw"></i> Bot.tf snapshots</a>
+            <a class="btn btn-default" href="${generateStatsLink(ItemData)}" target="_blank"><i class="fa fa-bar-chart fa-fw"></i> Bot.tf stats</a>
+        `);
+        $('.panel-extras').append(`
+            <a class="btn btn-panel" href="${generateSnapshotLink(ItemData)}" target="_blank"><i class="fa fa-history fa-fw"></i> Bot.tf snapshots</a>
+            <a class="btn btn-panel" href="${generateStatsLink(ItemData)}" target="_blank"><i class="fa fa-bar-chart fa-fw"></i> Bot.tf stats</a>
+        `);
+    }
+
+    /**
+     * Generate the URL parameters
+     * @param options
+     * @return {object}
+     */
+    function generateParams(options)
+    {
+        return Object.assign({
+            def: undefined,
+            q: 6,
+            ef: '',
+            craft: 1,
+            aus: 0,
+            ks: 0,
+            source: 'browser_script'
+        }, options);
+    }
+
+    /**
+     * Generate the link for a snapshot
+     * @param options
+     * @return {string}
+     */
+    function generateSnapshotLink(options)
+    {
+        options = generateParams(options);
+
+        return 'https://bot.tf/stats/listings?' +
+            $.param(options);
+    }
+
+    /**
+     * Generate the link for stats
+     * @param options
+     * @return {string}
+     */
+    function generateStatsLink(options)
+    {
+        options = generateParams(options);
+
+        return 'https://bot.tf/stats/item?' +
+            $.param(options);
+    }
+})($);
